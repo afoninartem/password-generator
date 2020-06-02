dda8403f953cf11d2a0309310f6ffd59556cc6de
afoninartem <47443887+afoninartem@users.noreply.github.com>
2020-06-02 23:58:37 +0300

    buttons changed and select added
    changed buttons to divs with text today
    also added select with length, now have to make it change text color after picking

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-(function() {
-  const buttons = Array.from(document.querySelectorAll('.btn'));
-  buttons.length = 4;
-  // console.log(buttons)
-  buttons.forEach(
-    button => button.addEventListener('click', function(e) {
-      console.log(this.target);
-    })
-  );
-}());
+const btns = Array.from(document.querySelectorAll('.btn'));
+btns.splice(btns.length-1);
+console.log(btns);
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+  })
+});
+
+const goActive = () => {
+  document.querySelector('#length').classList.add('active');
+}
